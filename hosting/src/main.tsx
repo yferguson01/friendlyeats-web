385e0901f4d1df98699ef37755f6c1658d5cd82c
Cynthia Wang <hyacccint@gmail.com>
2023-01-12 13:46:23 -0500

    fix: redact secret

--- hosting/src/main.tsx
+++ hosting/src/main.tsx
@@ -7,12 +7,6 @@ import "./index.css";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBk-u6F9UAesAwj7NQh80Rlr-AHc69UtOY",
-  authDomain: "rc-strawberry.firebaseapp.com",
-  projectId: "rc-strawberry",
-  storageBucket: "rc-strawberry.appspot.com",
-  messagingSenderId: "105225960024",
-  appId: "1:105225960024:web:a2c70ff84e835a7d22682d",
 };
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
